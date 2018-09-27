e9d7a82b57f500f5ee7b1acc24d91eed1386f7c5
WFrame0224 <wf0224@outlook.com>
2018-09-27 21:53:26 +0800

    增加了匿名内部类访问局部变量

--- day10/AnonymousInnerClassDemo1.java
+++ day10/AnonymousInnerClassDemo1.java
@@ -11,9 +11,13 @@ abstract class Device
 	{
 		return this.name;
 	}
+	public void accessPartFildes(){}
 }
 public class AnonymousInnerClassDemo1
 {
+	// 创建类成员
+	static int age = 20;
+	int age1 = 20;
 	// 传入一个Deive类型的变量
 	public void test(Device d)
 	{
@@ -21,6 +25,7 @@ public class AnonymousInnerClassDemo1
 	}
 	public static void main(String[] args) 
 	{
+		String var = "-----局部变量------";
 		// 创建一个实例
 		AnonymousInnerClassDemo1 ai = new AnonymousInnerClassDemo1();
 		// 调用有参数的构造器创建Device匿名实现类的对象
@@ -48,7 +53,31 @@ public class AnonymousInnerClassDemo1
 			{
 				return "键盘";
 			}
+			public void accessPartFildes()
+			{
+				// 从java8 以前，下面的语句将出错，var 必须使用final 修饰，
+				// 从java8 开始，匿名内部类允许访问非final修饰的内部变量，java8更加智能
+				System.out.println(var);
+				// 经过上一句话，匿名内部类访问了局部变量，该局部变量自动使用了final 修饰，因此初始化之后无法修改
+				//var = "更改你";
+				/**
+					AnonymousInnerClassDemo1.java:56: 错误: 从内部类引用的本地变量必须是最终变量或实际上的最终变量
+						var = "更改你";
+						^	
+					1 个错误
+				 */
+
+				System.out.println("访问的是外部类的静态成员变量" + age);
+				//System.out.println("访问的是外部类的非静态成员变量" + age1);//编译错误,这是因为作用域是static 修饰的main方法，说明匿名内部类的作用域和此方法是一样的
+				/**
+					AnonymousInnerClassDemo1.java:71: 错误: 无法从静态上下文中引用非静态 变量 age1
+								System.out.println("访问的是外部类的非静态成员变量" + age1);//编译错误
+				                                       ^
+										1 个错误
+				 */
+			}
 		};
+		d.accessPartFildes();
 		ai.test(d);
 	}
 } 
